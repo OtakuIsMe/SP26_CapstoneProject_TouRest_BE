c2298b7fa67167547cb1d814a0d0beb60c1e62b5
Hai124-coder <haihenry1124@gmail.com>
2026-04-08 16:45:08 +0700

    Add DTOs Package Service

---
+++ Application/DTOs/PackageService/PackageServiceDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TouRest.Domain.Enums;
+
+namespace TouRest.Application.DTOs.PackageService
+{
+    public class PackageServiceDTO
+    {
+        public Guid PackageId { get; set; }
+        public Guid ServiceId { get; set; }
+        public int SortOrder { get; set; }
+
+        public string ServiceName { get; set; } = null!;
+        public string? ServiceDescription { get; set; }
+        public int ServicePrice { get; set; }
+        public int ServiceDurationMinutes { get; set; }
+        public ServiceStatus ServiceStatus { get; set; }
+        public int ServiceBasePrice { get; set; }
+    }
+}
